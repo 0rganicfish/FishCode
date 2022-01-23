6eb628d7d923f9ae024992d7bf6b2bdfeb6eac48
OrganicFish <2827002857@qq.com>
2022-01-23 16:41:09 +0800

    1

--- CPP.code/cyy.cpp
+++ CPP.code/cyy.cpp
@@ -1,24 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-void Perm(int list[], int k, int m)
-{
-    if (k == m)
-    {
-        for (int i = 0; i <= m; i++)
-            cout << list[i] << " ";
-        cout << endl;
-    }
-    else
-        for (int i = k; i <= m; i++)
-        {
-            swap(list[i], list[k]);
-            Perm(list, k + 1, m);
-            swap(list[i], list[k]);
-        }
-}
+
 int main()
 {
-    int a[] = {1, 2, 3, 4, 5};
-    Perm(a, 0, 4);
-        
+
 }
