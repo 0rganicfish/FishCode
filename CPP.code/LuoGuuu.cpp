086fc5c41e69f6a1e5a018ee7a6622a0ddeffa56
OrganicFish <2827002857@qq.com>
2022-01-23 14:24:57 +0800

    1

--- CPP.code/LuoGuuu.cpp
+++ CPP.code/LuoGuuu.cpp
@@ -1,8 +1,31 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+int a[100020];
+int ans[100020][2];
 
 int main()
 {
-    cout << "423";
+    int T, n;
+    scanf("%d", &T);
+    while (T--)
+    {
+        scanf("%d", &n);
+        for (int i = 1; i <= n; ++i)
+            scanf("%d", a + i);
+        int as = 0, cnt = 0;
+        for (int i = 1, j = 1; i <= n; i = ++j)
+        {
+            while (j < n && a[j] <= a[j + 1])
+                ++j;
+            if (a[j] - a[i] > as)
+                as = a[j] - a[i], cnt = 1, ans[1][0] = i, ans[1][1] = j;
+            else if (a[j] - a[i] == as)
+                ans[++cnt][0] = i, ans[cnt][1] = j;
+        }
+        for (int i = 1; i <= cnt; ++i)
+            printf("%d %d ", ans[i][0], ans[i][1]);
+        printf("\n");
+    }
     return 0;
 }
--- CPP.code/cyy.cpp
+++ CPP.code/cyy.cpp
@@ -1,46 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-int month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-bool run(int x)
+void Perm(int list[], int k, int m)
 {
-    if ((x % 4 == 0 and x % 100) or x % 400 == 0)
-        return true;
-    return false;
-}
-bool hui(string s)
-{
-    for (int i = 0, l = s.length(); i < l; i++)
-        if (s[i] != s[l - i - 1])
-            return false;
-    return true;
-}
-string addyear(int x)
-{
-    int y = x / 10000, m = x / 100 % 100, d = x % 100;
-    if (run(y))
-        month[2] = 29;
+    if (k == m)
+    {
+        for (int i = 0; i <= m; i++)
+            cout << list[i] << " ";
+        cout << endl;
+    }
     else
-        month[2] = 28;
-    if (++d > month[m])
-        m++, d = 1;
-    if (m > 12)
-        m = 1, y++;
-    string ans = to_string(y * 10000 + m * 100 + d);
-    if (y < 10)
-        ans = "000" + ans;
-    else if (y >= 10 and y < 100)
-        ans = "00" + ans;
-    else if (y >= 100 and y < 1000)
-        ans = "0" + ans;
-    return ans;
+        for (int i = k; i <= m; i++)
+        {
+            swap(list[i], list[k]);
+            Perm(list, k + 1, m);
+            swap(list[i], list[k]);
+        }
 }
-
 int main()
 {
-    int n, m;
-    string t;
-    cin >> n >> m;
-    for (int i = n; i <= m; t = addyear(i), i = stoi(t))
-        if (hui(t))
-            cout << t << endl;
+    int a[] = {1, 2, 3, 4, 5};
+    Perm(a, 0, 4);
+        
 }
