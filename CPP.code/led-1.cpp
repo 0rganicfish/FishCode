5f2f9a601432108941a9d6a5091b098d07ae3622
OrganicFish <2827002857@qq.com>
2022-01-29 22:17:38 +0800

    1

--- CPP.code/led-1.cpp
+++ CPP.code/led-1.cpp
@@ -1,8 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+inline int Pow(int x, int n)
+{
+    int ans = x;
+    while (--n)
+        ans *= x;
+    return ans;
+}
+
 int main()
 {
-    
+    int n;
+    scanf("%d", &n);
+    int m = pow(10, n - 1);
+    for (int i = m; i < 10 * m; i++)
+    {
+        int sum = 0, t = i;
+        while (t)
+            sum += Pow(t % 10, n), t /= 10;
+        if (sum == i)
+            printf("%d\n", i);
+    }
     return 0;
 }
