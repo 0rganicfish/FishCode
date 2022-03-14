1a2105d55580599828643556503ff3f9c55e76af
OrganicFish <2827002857@qq.com>
2022-03-15 00:35:39 +0800

    1

--- CPP/led-1.cpp
+++ CPP/led-1.cpp
@@ -1,7 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+
+bool v[10000];
+int n, m, i, j, sum = 0, k;
+
+bool isprime(int n)
+{
+    for (i = 2; i <= sqrt(n); i++)
+        if (n % i == 0)
+            return false;
+    return true;
+}
+int a[25];
+long long ans;
+void dfs(int m, int sum, int startx)
+{
+    if (m == k)
+    {
+        if (isprime(sum))
+            ans++;
+        return;
+    }
+    for (int i = startx; i < n; i++)
+        dfs(m + 1, sum + a[i], i + 1);
+    return;
+}
 
 int main()
 {
-
+    scanf("%d%d", &n, &k);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    dfs(0, 0, 0);
+    printf("%d\n", ans);
+    return 0;
 }
